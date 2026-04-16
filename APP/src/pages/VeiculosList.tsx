3fef13253cda5da6a33bf5c076dca65d2dd8e67e
pedro8etec <predo.etec1@gmail.com>
2026-04-16 11:40:25 +0000

    aura

--- APP/src/pages/VeiculosList.tsx
+++ APP/src/pages/VeiculosList.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { getVeiculos } from "../services/veiculoService";
 import type { Veiculo } from "../types/veiculo";
 import { VeiculoCard } from "../components/VeiculoCard";
+import { ListaVazia } from "../components/ListaVazia";
 
 export function VeiculosList() {
   const [veiculos, setVeiculos] = useState<Veiculo[]>([]);
@@ -14,14 +15,18 @@ export function VeiculosList() {
     <div>
       <h1>Lista de Veículos</h1>
 
-      {veiculos.map((v) => (
-        // <div key={v.id}>
-        //   <h2>{v.modelo}</h2>
-        //   <p>{v.descricao}</p>
-        //   <strong>R$ {v.valor}</strong>
-        // </div>
-        <VeiculoCard key={v.id} veiculo={v} />
-      ))}
+      {veiculos.length === 0 ? (
+        <ListaVazia />
+      ) : (
+        veiculos.map((v) => (
+          // <div key={v.id}>
+          //   <h2>{v.modelo}</h2>
+          //   <p>{v.descricao}</p>
+          //   <strong>R$ {v.valor}</strong>
+          // </div>
+          <VeiculoCard key={v.id} veiculo={v} />
+        ))
+      )}
     </div>
   );
 }
